eadb96bdca7622eed55b3a154437ccc00757a952
swapnanildeb <swapnanildeb3@gmail.com>
2020-01-27 19:21:10 -0600

    tagger changes

---
+++ Project 1/tagger.py
@@ -0,0 +1,15 @@
+import nltk
+
+def tagger(tweet):
+	tokens = nltk.word_tokenize(sentence)
+	print("HELLO")
+	print(tokens)
+
+def main():
+	print("wuh")
+	#tagger("hello world yo gang yuh")
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
