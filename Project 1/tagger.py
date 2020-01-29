eea0a20ba271d033491830e3c8c0cfc94de99640
swapnanildeb <swapnanildeb3@gmail.com>
2020-01-29 16:14:02 -0600

    tagger updates

--- Project 1/tagger.py
+++ Project 1/tagger.py
@@ -1,13 +1,43 @@
-import nltk
+import imdb
+import spacy
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
+ia = imdb.IMDB()
+
+def actor_filter(ent):
+	white_list = ['PERSON', 'WORK_OF_ART', 'DATE']
+	if(ent.label_ in white_list):
+		return True
+	else:
+		return False
+
+def movie_filter(ent):
+	white_list = ['PERSON', 'WORK_OF_ART', 'DATE']
+	if(ent.label_ in white_list):
+		return True
+	else:
+		return False
+
+def actor_check
 
 def tagger(tweet):
-	tokens = nltk.word_tokenize(sentence)
-	print("HELLO")
-	print(tokens)
+	doc = nlp(tweet)
+	#print([(X.text, X.label_) for X in doc.ents])
+	actor_bool, movie_bool = False
+	#print([(X, X.ent_iob_, X.ent_type_) for X in doc])
+	filtered = filter(ent_filter, doc.ents)
+
+	for X in filtered:
+		print(X.text)
+
 
 def main():
-	print("wuh")
-	#tagger("hello world yo gang yuh")
+
+	#tagger("‘1917’ and ‘Once Upon a Time… in Hollywood’ take top prizes at Golden Globes")
+	tagger("henlo bitches")
 
 if __name__ == "__main__":
     # execute only if run as a script
