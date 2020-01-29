c07e203616046a0050416fced7e1bd969ab1f5e5
Saubhagya Shrestha <saubhagyashrestha2021@u.northwestern.edu>
2020-01-29 13:43:27 -0600

    Just fixed the key work search function

--- Project 1/prune.py
+++ Project 1/prune.py
@@ -1,34 +1,32 @@
 import json
 from classes import *
-
 from fuzzywuzzy import fuzz
-
-
-str1 = "Priyanka Chopra and Nick Jonas at the 77th Annual Golden Globe Awards presented the award for Best TV series for a musical or comedy"
-#str2 = "the best tv series award was presented by nick jonas and his wife"
-str3 = "Priyanka Chopra and Nick Jonas at the 77th Annual Golden Globe Awards won the award for Best TV series for a musical or comedy"
+import string
 
 
 def contains_key_words(text):
     #this list of words needs to be added to
-    key_words = ["win", "nominee", "nominated", "congrat", "best", "award", "goes to", "present", "host"]
-    text = text.split()
+    key_words = ["best", "award", "goes to", "present", "host", "monologue"]
+    #prior list of key words: "win", "wins", "winner","won","nominee", "nominated", "congrat", "best", "award", "goes to", "present", "host", "monologue"
+    text1 = text.lower().translate(str.maketrans('', '', string.punctuation)).split()
     for i in key_words:
-        for j in text:
-            if fuzz.patial_ratio(i, j.lower()) > 55:
-                return True
+        if i in text1:
+            print("Found Relevant: " + i + " \n" + "Tweet: " + text + "\n\n")
+            return True
     return False
+
+def tagger(tweet):
+    return(Tweet(tweet,[]))
 
 def prune():
     TweetObjs = []
-    with open('gg2020.json', encoding="utf8") as f:
+    with open('test.json', encoding="utf8") as f:
         for line in f:
             message = json.loads(line)
             #get the text
             tweet = message["text"]
             #pass text through tagger
             tweetobj = tagger(tweet)
-
-
             if len(tweetobj.tweet_tags) != [] and contains_key_words(tweetobj.tweet_text):
                 TweetObjs.append(tweetobj)
+    return(TweetObjs)
