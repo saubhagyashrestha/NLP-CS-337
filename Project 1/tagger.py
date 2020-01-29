4c2740c6865bd4a9a9848ebefc80e69a5544ddac
swapnanildeb <swapnanildeb3@gmail.com>
2020-01-29 16:38:58 -0600

    tagger finished

--- Project 1/tagger.py
+++ Project 1/tagger.py
@@ -1,43 +1,53 @@
-import imdb
+from imdb import IMDb
 import spacy
 from spacy import displacy
 from collections import Counter
 import en_core_web_sm
 
 nlp = en_core_web_sm.load()
-ia = imdb.IMDB()
+ia = IMDb()
 
 def actor_filter(ent):
-	white_list = ['PERSON', 'WORK_OF_ART', 'DATE']
-	if(ent.label_ in white_list):
-		return True
+	if(ent.label_ == 'PERSON'):
+		people = ia.search_person(ent.text)
+		if(people[0] == ent.text):
+			return True
+		else:
+			return False
 	else:
 		return False
 
 def movie_filter(ent):
-	white_list = ['PERSON', 'WORK_OF_ART', 'DATE']
+	white_list = ['WORK_OF_ART', 'DATE']
 	if(ent.label_ in white_list):
-		return True
+		movies = ia.search_movie(ent.text)
+		if(str(movies[0]) == str(ent.text) and movies[0]['year'] == 2019):
+			return True
 	else:
 		return False
-
-def actor_check
 
 def tagger(tweet):
 	doc = nlp(tweet)
 	#print([(X.text, X.label_) for X in doc.ents])
-	actor_bool, movie_bool = False
+	actor_bool= False
+	movie_bool = False
 	#print([(X, X.ent_iob_, X.ent_type_) for X in doc])
-	filtered = filter(ent_filter, doc.ents)
-
-	for X in filtered:
-		print(X.text)
+	actor_filtered = list(filter(actor_filter, doc.ents))
+	movie_filtered = list(filter(movie_filter, doc.ents))
+	if(len(actor_filtered) > 0 and len(movie_filtered) > 0):
+		return ['actor', 'movie']
+	if(len(actor_filtered) > 0):
+		return ['actor']
+	if(len(movie_filtered) > 0):
+		return ['movie']
+	return []
 
 
 def main():
 
 	#tagger("‘1917’ and ‘Once Upon a Time… in Hollywood’ take top prizes at Golden Globes")
-	tagger("henlo bitches")
+	tags = tagger("1917")
+	print(tags)
 
 if __name__ == "__main__":
     # execute only if run as a script
