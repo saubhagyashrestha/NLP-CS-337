3f421d4af115cfd6db917a0ce6c561fff5627744
Emily <eajenkins405@gmail.com>
2020-02-08 22:09:49 -0600

    Saubhagya and I worked on get_awards_new. It works decent now. Also intergrated everything with the autograder to make sure everything works okay. Igonre get_awards.py from now on.

--- Project 1/get_awards_new.py
+++ Project 1/get_awards_new.py
@@ -3,66 +3,316 @@ import spacy
 from spacy import displacy
 from collections import Counter
 import en_core_web_sm
-import nltk
+from nltk import word_tokenize, pos_tag
+import string
+import re
+from collections import deque
+import time
+from emoji import UNICODE_EMOJI
 
 nlp = en_core_web_sm.load()
-#takes a file name and gets a list of awards from it
-def get_awards():
-    relevant_tweets = []
 
-    with open('pruned_tweets.json',encoding="utf8") as infile:
+def get_tags(sentence):
+    tokens = word_tokenize(sentence)
+    tags = pos_tag(tokens)
+    return [x[1] for x in tags]
+
+def has_emoji(word):
+    for char in word:
+        if char in UNICODE_EMOJI:
+            return True
+    return False
+
+
+def get_awards(year):
+    year = str(year)
+    OFFICIAL_AWARDS = []
+    if year == '2020':
+        OFFICIAL_AWARDS = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama',
+        'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy',
+        'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture',
+        'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture',
+        'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture',
+        'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television',
+        'best performance by an actress in a limited series or a motion picture made for television',
+        'best performance by an actor in a limited series or a motion picture made for television',
+        'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama',
+        'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy',
+        'best performance by an actress in a supporting role in a series, limited series or motion picture made for television',
+        'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award', 'carol burnett award']
+    else:
+        OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+
+    ll = []
+    for i in OFFICIAL_AWARDS:
+        lll = pos_tag(word_tokenize(i))
+        for ii in lll:
+            if ii[1] not in ll:
+                ll.append(ii[1])
+    print(ll)
+
+    lst = []
+    print("**************STARTED " + year + "********************")
+    startt = time.time()
+
+    file_name = 'pruned_tweets_best_' + year +'.json'
+    with open(file_name,encoding="utf8") as infile:
         for line in infile:
             text = json.loads(line)['text']
-            relevant_tweets.append(text)
+            lst.append(text.lower())
 
 
     infile.close()
+    point = time.time()
+    print(point-startt, " seconds since the start")
 
-    best = "best"
-    awarded_words = ["best", "congrats"]
-    verb_words_entity_first = ["won", "wins", "winner", "got"]
-    verb_words_entity_second = ["goes to", "awarded to"]
+    print(len(lst))
 
-    example = "Sad about paul rudd but happy that emily jenkins got best director"
-    example2 = "best picture animated goes to frozen 2"
-    example1 = "happy that emily got best director"
+    verb_conj = ["goes", "awarded", "went", "to", "and", "but", "at", "with", "on", "is", "https", "http", "win", "won", "wins", "award", "...", "#"]
+    in_word = ['http']
+    puncs = string.punctuation.replace("-","").replace(",","")
+    one_b4 = ['his','her','them']
+    cut = ["\n"]
+    banned = ["of", "on", "from", "golden", "tv", "over", "into", "globe", "globes"]
+    banned_char = "@&()';:/’" + '"'
+    #verb_words_entity_first = ["won", "wins", "winner", "got"]
 
-    token = nltk.word_tokenize(example)
-    print(nltk.pos_tag(token)[example.split().index('emily')])
+    potential_awards = {}
+    for tweet in lst:
+        slt = tweet.split()
+        i,start = 0,0
+        b_huh = False
+        already = False
+        while i < len(slt):
+            wrd = slt[i]
+            if b_huh and (',' in wrd or '-' in wrd):
+                if already:
+                    p_name = " ".join(slt[start:i])
+                    if p_name in potential_awards:
+                        potential_awards[p_name] += 1
+                    else:
+                        potential_awards[p_name] = 1
+                    b_huh = False
+                    already = False
+                else:
+                    already = True
+            if b_huh and wrd in banned:
+                b_huh = False
+                already = False
+            if b_huh:
+                for char in banned_char:
+                    if char in wrd:
+                        b_huh = False
+                        already = False
+                        break
+            if has_emoji(wrd):
+                b_huh = False
+                already = False
+            if wrd == "best":
+                if b_huh:
+                    p_name = " ".join(slt[start:i])
+                    if p_name in potential_awards:
+                        potential_awards[p_name] += 1
+                    else:
+                        potential_awards[p_name] = 1
+                b_huh = True
+                start = i
+            if b_huh and wrd[-1] in puncs:
+                p_name = " ".join(slt[start:i+1])
+                while p_name[-1] in puncs:
+                    p_name = p_name[:-1]
+                if p_name in potential_awards:
+                    potential_awards[p_name] += 1
+                else:
+                    potential_awards[p_name] = 1
+                b_huh = False
+                already = False
+            if b_huh and wrd[-2:] == '\n':
+                p_name = " ".join(slt[start:i])[:-2]
+                if p_name in potential_awards:
+                    potential_awards[p_name] += 1
+                else:
+                    potential_awards[p_name] = 1
+                b_huh = False
+                already = False
+            if b_huh and '#' == wrd[0]:
+                p_name = " ".join(slt[start:i])
+                if p_name in potential_awards:
+                    potential_awards[p_name] += 1
+                else:
+                    potential_awards[p_name] = 1
+                b_huh = False
+                already = False
+            if b_huh and wrd in verb_conj:
+                p_name = " ".join(slt[start:i])
+                if p_name in potential_awards:
+                    potential_awards[p_name] += 1
+                else:
+                    potential_awards[p_name] = 1
+                b_huh = False
+            if b_huh and wrd in one_b4:
+                p_name = " ".join(slt[start:i-1])
+                if p_name in potential_awards:
+                    potential_awards[p_name] += 1
+                else:
+                    potential_awards[p_name] = 1
+                b_huh = False
+            if b_huh and 'http' in wrd:
+                p_name = " ".join(slt[start:i])
+                if p_name in potential_awards:
+                    potential_awards[p_name] += 1
+                else:
+                    potential_awards[p_name] = 1
+                b_huh = False
+            i += 1
+            if i == len(slt) and b_huh:
+                p_name = " ".join(slt[start:i])
+                if p_name in potential_awards:
+                    potential_awards[p_name] += 1
+                else:
+                    potential_awards[p_name] = 1
 
-    for word in awarded_words:
-        if word in example:
-            print("found " + word + "\n")
-            ent_list = nlp(example).ents
-            for wordz in verb_words_entity_first:
-                if wordz in example:
-                    for person in ent_list:
-                        if person.label_ == 'PERSON':
-                            if example.index(person.text) < example.index(wordz):
-                                print("This person may have gotten an award: ", person.text)
+
+    point = time.time()
+
+    print(point-startt, " seconds to do trim")
+    print("There are ", len(potential_awards), " potential awards")
 
 
-    for i in [example1, example2]:
-        if best in i:
-            start = i.index(best)
-            print("found " + best + "\n")
-            for word in verb_words_entity_second:
-                if word in i:
-                    end = i.index(word)
-                    print(i[start:end])
-            for word in verb_words_entity_first:
-                if word in i:
-                    end = len(i)
-                    print(i[start:end])
 
-                        #indx = example.index(wordz)
-                        #str = ''
-                        #print(nltk.pos_tag(example[indx]))
-                        #while (indx > -1 and nlp(example[indx]).ents and nlp(example[indx]).ents[0].label_ == 'PERSON'):
-                        #while (indx > -1 and nlp(example[indx]).ents and nlp(example[indx]).ents[0].label_ == 'PERSON'):
-                        #    str = example[indx] + " " + str
-                        #    indx = indx - 1
-                        #    print(indx)
-                        #    print(str)
+    countt = 0
+    print("HERE are the direct matches: ")
+    for name,value in potential_awards.items():
+        if name in OFFICIAL_AWARDS:
+            print(name,value)
+            countt += 1
+    print("There are ", countt, " of them.")
 
-get_awards()
+    p_names = []
+    booger = []
+    for name,value in potential_awards.items():
+        splitted = name.split()
+
+        for o_name in OFFICIAL_AWARDS:
+            if o_name in name and o_name not in booger:
+                booger.append(o_name)
+            if o_name in name and o_name != name:
+                print("Improve trim: ", name)
+
+        if value > 1 and len(splitted) > 3 and pos_tag(word_tokenize(splitted[1]))[0][1] in ['NN', 'VBN', 'JJ', 'NNS']:
+            p_names.append([name,value])
+
+    point = time.time()
+    print(point-startt, " seconds since the start")
+
+    print("Potential award names ending determined by verb_conj, puncuation, pronouns")
+    print("In the potential names, there exists " , len(booger), " perfect award names somewhere in there")
+
+    p_names.sort(key = lambda x: -x[1])
+
+    countt = 0
+    print("HERE are the direct matches: ")
+    for x in p_names:
+        if x[0] in OFFICIAL_AWARDS:
+            print(x)
+            countt += 1
+    print("There are ", countt, " of them.")
+
+
+    print("\nWe have a total of ", len(p_names), " potential awards.\nHere are the top half of them: ")
+    for ind in range(int(len(p_names)/2)):
+        print("FIRST CUT: ", p_names[ind])
+
+#############################################################################################################################################
+    #prune list of possible awards
+
+    possibletags = set(['JJS', 'NN', ':', 'JJ', 'CC', 'RBS', 'IN', 'DT', 'VBN', ',', 'VBZ', 'RB', 'NNS'])
+    banned = {"of", "on", "from", "golden", "tv", "win"}
+    newlst = []
+    for pair in p_names:
+        tags = get_tags(pair[0])
+        flag = True
+        for tag in tags:
+            if tag not in possibletags:
+                flag = False
+                break
+        if flag:
+            ### my part hello
+            if len(set(pair[0].split()).intersection(banned)) < 1:
+                last_word_tag = get_tags(pair[0].split()[-1])
+                if 'NN' in last_word_tag or 'VBN' in last_word_tag or 'JJ' in last_word_tag:
+                    if pair[0][-1] != "–" and pair[0][-1] != "," and pair[0][-1] != " ":
+                        newlst.append(pair)
+
+    print("\nWe have now taken out awards with specific words such as on, from, golden, globes")
+    print("Last word also has to be a noun and cannot end in a punctuation")
+    point = time.time()
+    print(point-startt, " seconds since the start")
+
+    countt = 0
+    print("HERE are the direct matches: ")
+    for x in newlst:
+        if x[0] in OFFICIAL_AWARDS:
+            print(x)
+            countt += 1
+    print("There are ", countt, " of them.")
+
+    print("\nWe have a total of ", len(newlst), " potential awards.\nHere are the top half of them: ")
+    for ind in range(len(newlst)):
+        print("SECOND CUT: ", newlst[ind])
+
+#############################################################################################################################################
+
+    lets_see = set(["-", "for", "in", "by"])
+    newlst_key = []
+    for item in newlst:
+        if len(item[0].split()) < 7:
+            item[1] += len(set(item[0].split()).intersection(lets_see))*20
+        else:
+            item[1] += len(set(item[0].split()).intersection(lets_see))*15
+    newlst.sort(key = lambda x: -x[1])
+
+    newlst_key = newlst[:27]
+
+    print("\n\nModified list!! by adding value for keywords")
+    point = time.time()
+    print(point-startt, " seconds since the start")
+
+    print("There are not a total of ", len(newlst_key), " of them.\n Here they are: ")
+    for x in newlst_key:
+        print(x)
+
+#############################################################################################################################################
+
+    print("\n\nTagging them with spacy now...")
+    newlst_key_spac = []
+    for x in newlst_key:
+        if nlp(x[0]).ents:
+            pass
+        else:
+            newlst_key_spac.append(x)
+
+    point = time.time()
+    print(point-startt, " seconds since the start")
+    print("There are not a total of ", len(newlst_key_spac), " of them.\n Here they are: ")
+    for x in newlst_key_spac:
+        print(x)
+
+#############################################################################################################################################
+
+    matches = []
+    for i in OFFICIAL_AWARDS:
+        for j in newlst_key:
+            if i == j[0]:
+                matches.append(j)
+
+    print("\nThere are now ", len(matches), " perfect matches.")
+    for pair in matches: #.items():
+        print("Found perfect match for : ",  pair[0], "Value: ", pair[1])
+    print("\n")
+    names = [x[0] for x in matches]
+    for jj in OFFICIAL_AWARDS:
+        if jj not in names:
+            print("not found: ", jj)
+
+    return names
--- Project 1/prune.py
+++ Project 1/prune.py
@@ -1,5 +1,4 @@
 import json
-from tagger import tagger
 from classes import *
 import string
 
@@ -7,44 +6,51 @@ import string
 def contains_key_words(text):
     #this list of words needs to be added to
     key_words = ["wins", "win", "won", "winner", "nominate", "nomimated", "nominee", "lost", "award", "awarded", "awards", "present", "presenter", "presents", "presented", "host",
-    "hosts", "hosted","best", "goes to", "gave", "announce", "announced", "introduce", "introduced", "monologue", "carol", "brunett","cecil","demille"]
-    #prior list of key words: "win", "wins", "winner","won","nominee", "nominated", "congrat", "best", "award", "goes to", "present", "host", "monologue"
-
-    award_words = ["motion","picture","screenplay, ""movie","tv","television","series","performance","director","actor","actress","drama","comedy","animated","foreign","original","score",
-    "song","cecil","demille","musical","role","supporting","limited","carol","burnett","best","language","award"]
+    "hosts", "hosted","best", "goes to", "gave", "announce", "announced", "introduce", "introduced", "monologue", "carol", "brunett","cecil","demille","best", "award"]
 
     text = text.split()
     for i in key_words:
-        if i in text and len(set(text).intersection(set(award_words))) > 3:
+        if i in text: # and len(set(text).intersection(set(award_words))) > 3:
             return True
     return False
 
-def tag_wrapper(tweet):
-    tags = tagger(tweet)
-    return(tags)
-
-def prune(file_name):
+def prune(year):
+    year = str(year)
     pruned_tweets = []
+    pruned_tweets_best = []
+    file_name = 'gg' + year + '.json'
     with open(file_name,encoding="utf8") as infile:
+        dic = {}
         for line in infile:
-            message = json.loads(line)
+            dic = json.loads(line)
+            #get the text
+        for message in dic:
+            #message = json.loads(line)
             #get the text
             text = message["text"]
-            text = text.lower().translate(str.maketrans('', '', string.punctuation.replace("-", "")))
+            text1 = text.lower().translate(str.maketrans('', '', string.punctuation.replace("-", "")))
             #pass text through tagger
             #if len(tweetobj.tweet_tags) != [] and contains_key_words(tweetobj.tweet_text):
-            if contains_key_words(text):
-                tags = tag_wrapper(text)
+            if contains_key_words(text1):
                 tweet_dic = {}
                 tweet_dic['text'] = text
-                tweet_dic['tags'] = tags
                 pruned_tweets.append(tweet_dic)
+            if 'best' in text1.split():
+                tweet_dic = {}
+                tweet_dic['text'] = text
+                pruned_tweets_best.append(tweet_dic)
     infile.close()
 
-    with open('pruned_tweets.json','w') as outfile:
+    name = 'pruned_tweets_' + year + '.json'
+    with open(name,'w') as outfile:
         for tweet in pruned_tweets:
             json.dump(tweet, outfile)
             outfile.write('\n')
     outfile.close()
 
-prune('gg2020.json')
+    name2 = 'pruned_tweets_best_' + year + '.json'
+    with open(name2,'w') as outfile:
+        for tweet in pruned_tweets_best:
+            json.dump(tweet, outfile)
+            outfile.write('\n')
+    outfile.close()
